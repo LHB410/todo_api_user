3f96e972ed0263ba7b39e2480ff73ad0aecd8a31
Laura Brooks <laura.brooks0@outlook.com>
2022-09-06 22:33:06 +0900

    initial commit

---
+++ src/TodoList/TodoForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import TextField  from "@mui/material/TextField";
+import  Button  from "@mui/material/Button";
+
+class TodoForm extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        api_url: props.api_url,
+        task: "",
+      }
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleTaskChange = this.handleTaskChange.bind(this);
+
+    }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      this.formSubmit(event.target);
+    }
+
+    async formSubmit(formData) {
+      var data = new FormData(formData)
+      await fetch(this.state.api_url, {
+        method: "POST",
+        mode: "cors",
+        body: data
+      }).then(response => response.json)
+      .then(response => this.props.updateTodoList(response))
+    }
+
+    handleTaskChange(event) {
+      this.setState({
+        task: event.target.value
+      })
+    }
+
+    render () {
+      return (
+        <div>
+          <form onSubmit = {this.handleSubmit} id="todo_form" autoComplete="off">
+
+            <TextField
+              id="task_input"
+              label="Task description"
+              variant="outlined"
+              type="text"
+              name="todo[task]"
+              onChange={this.handleTaskChange}/>
+
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit" >Add Task</Button>
+          </form>
+        </div>
+      )
+    }
+}
+export default TodoForm;
